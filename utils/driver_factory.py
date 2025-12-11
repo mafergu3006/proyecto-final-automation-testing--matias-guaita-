1ccd789e3efa3eb0a0ae47ca9199bbf82f1290c0
mafergu3006 <167816603+mafergu3006@users.noreply.github.com>
2025-12-11 00:52:44 -0300

    Refactor driver initialization with DriverFactory
    Refactor driver creation to use DriverFactory class and load configuration from YAML.

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,10 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import yaml
 
-def get_driver(browser="chrome"):
-    if browser == "chrome":
-        options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
-        return webdriver.Chrome(options=options)
-    else:
-        raise ValueError("Browser not supported")
+class DriverFactory:
 
+    @staticmethod
+    def get_driver():
+        with open("config/config.yaml", "r") as f:
+            config = yaml.safe_load(f)
+
+        browser = config["browser"]
+
+        if browser == "chrome":
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+        driver.implicitly_wait(config["implicit_wait"])
+
+        return driver
