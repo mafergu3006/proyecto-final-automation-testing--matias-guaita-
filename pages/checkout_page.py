06cd6b76f03c2fab5a890847b8c6654195d0f03b
mafergu3006 <167816603+mafergu3006@users.noreply.github.com>
2025-12-11 01:06:13 -0300

    Update checkout_page.py

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -3,21 +3,21 @@ from pages.base_page import BasePage
 
 class CheckoutPage(BasePage):
 
-    FIRSTNAME = (By.ID, "first-name")
-    LASTNAME = (By.ID, "last-name")
-    ZIP = (By.ID, "postal-code")
-    CONTINUE = (By.ID, "continue")
-    FINISH = (By.ID, "finish")
-    TITLE = (By.CLASS_NAME, "complete-header")
+    FIRST_NAME = (By.ID, "first-name")
+    LAST_NAME = (By.ID, "last-name")
+    ZIP_CODE = (By.ID, "postal-code")
+    CONTINUE_BTN = (By.ID, "continue")
+    FINISH_BTN = (By.ID, "finish")
+    CONFIRM_MSG = (By.CLASS_NAME, "complete-header")
 
-    def fill_form(self, name, last, zip_code):
-        self.type(self.FIRSTNAME, name)
-        self.type(self.LASTNAME, last)
-        self.type(self.ZIP, zip_code)
-        self.click(self.CONTINUE)
+    def fill_form(self, first, last, zip_code):
+        self.type(self.FIRST_NAME, first)
+        self.type(self.LAST_NAME, last)
+        self.type(self.ZIP_CODE, zip_code)
+        self.click(self.CONTINUE_BTN)
 
-    def finish_order(self):
-        self.click(self.FINISH)
+    def finish_purchase(self):
+        self.click(self.FINISH_BTN)
 
-    def get_confirmation(self):
-        return self.get_text(self.TITLE)
+    def purchase_successful(self):
+        return "Thank you" in self.find(self.CONFIRM_MSG).text
