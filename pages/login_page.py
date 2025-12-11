73475a81f567c84bb290421708858d6fafd6af52
mafergu3006 <167816603+mafergu3006@users.noreply.github.com>
2025-12-11 01:06:29 -0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,17 +1,22 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from utils.logger import logger
 
 class LoginPage(BasePage):
-
     USERNAME = (By.ID, "user-name")
     PASSWORD = (By.ID, "password")
     LOGIN_BTN = (By.ID, "login-button")
     ERROR_MSG = (By.XPATH, "//h3[@data-test='error']")
 
     def login(self, username, password):
+        logger.info(f"Ingresando usuario: {username}")
         self.type(self.USERNAME, username)
+        logger.info("Ingresando contraseña")
         self.type(self.PASSWORD, password)
+        logger.info("Haciendo clic en login")
         self.click(self.LOGIN_BTN)
 
     def get_error_message(self):
-        return self.get_text(self.ERROR_MSG)
+        msg = self.get_text(self.ERROR_MSG)
+        logger.info(f"Mensaje de error obtenido: {msg}")
+        return msg
