b2efe183a129c5ae7ed8c18d023e54acc554a00f
mafergu3006 <167816603+mafergu3006@users.noreply.github.com>
2025-12-11 00:57:32 -0300

    Refactor checkout test to use pytest and parameterization

--- tests/ui/test_checkout.py
+++ tests/ui/test_checkout.py
@@ -1,21 +1,42 @@
+import pytest
+import yaml
+from utils.driver_factory import DriverFactory
+from utils.data_reader import read_json
 from pages.login_page import LoginPage
-from pages.products_page import ProductsPage
+from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage
 from pages.checkout_page import CheckoutPage
 
-def test_checkout_flow(driver):
-    login = LoginPage(driver)
-    login.login("standard_user", "secret_sauce")
+checkout_data = read_json("data/checkout_data.json")
 
-    products = ProductsPage(driver)
-    products.add_product("Sauce Labs Bike Light")
-    products.go_to_cart()
+@pytest.fixture
+def driver():
+    driver = DriverFactory.get_driver()
+    yield driver
+    driver.quit()
 
+@pytest.mark.parametrize("data", checkout_data)
+def test_checkout_flow(driver, data):
+    with open("config/config.yaml") as f:
+        config = yaml.safe_load(f)
+
+    driver.get(config["base_url"])
+
+    # login
+    LoginPage(driver).login("standard_user", "secret_sauce")
+
+    # add product
+    inventory = InventoryPage(driver)
+    inventory.add_first_product_to_cart()
+    inventory.go_to_cart()
+
+    # go to checkout
     cart = CartPage(driver)
-    cart.checkout()
+    cart.proceed_to_checkout()
 
+    # fill checkout form
     checkout = CheckoutPage(driver)
-    checkout.fill_form("Juan", "Perez", "12345")
-    checkout.finish_order()
+    checkout.fill_form(data["first_name"], data["last_name"], data["zip"])
+    checkout.finish_purchase()
 
-    assert checkout.get_confirmation() == "Thank you for your order!"
+    assert checkout.purchase_successful()
